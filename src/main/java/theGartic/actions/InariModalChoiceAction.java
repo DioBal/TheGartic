d86c72aa995e75135d5c57b46ac36c9d2f604f1f
Cláuvin <Clauvin@users.noreply.github.com>
2022-03-20 18:08:13 -0300

    [SupplicateToInari] Added InariModalChoiceAction

---
+++ src/main/java/theGartic/actions/InariModalChoiceAction.java
@@ -0,0 +1,14 @@
+package theGartic.actions;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+import java.util.ArrayList;
+
+public class InariModalChoiceAction extends EasyModalChoiceAction {
+
+    public InariModalChoiceAction(ArrayList<AbstractCard> list, int amount, String textforSelect) {
+        super(list, amount, textforSelect);
+    }
+
+
+}
