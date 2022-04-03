ae7b0038c42cc66b03f83792935daa5d771e5013
Cláuvin <Clauvin@users.noreply.github.com>
2022-04-03 11:42:14 -0300

    [SupplToInari] Added Inari Flames

--- src/main/java/theGartic/cards/InariModal/InariFlames.java
+++ src/main/java/theGartic/cards/InariModal/InariFlames.java
@@ -3,12 +3,14 @@ package theGartic.cards.InariModal;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.powers.EnergizedPower;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.MonsterGroup;
 import theGartic.cards.EasyModalChoiceCard;
 
 import static theGartic.GarticMod.makeID;
@@ -21,33 +23,83 @@ public class InariFlames extends EasyModalChoiceCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
+    private static Runnable theOtherRunnableOnUseOrChosen;
+
     public InariFlames() {
-        super(NAME, DESCRIPTION, () -> {
+        super(NAME, DESCRIPTION, null);
+        Runnable runnable = ()->{
+
             AbstractPlayer p = AbstractDungeon.player;
             AbstractGameAction.AttackEffect effect = AbstractGameAction.AttackEffect.FIRE;
             DamageInfo.DamageType damageType = DamageInfo.DamageType.THORNS;
-            for (int i = 0; i < 10; i++){
-                atb(new DamageAction(m, new DamageInfo(p, 2, damageType), effect));
+            AbstractMonster m = GetAliveMonsterWithLessHealth();
+            if (m != null){
+                for (int i = 0; i < 10; i++){
+                    atb(new DamageAction(m, new DamageInfo(p, 2, damageType), effect));
+                }
             }
-
-        });
+        };
+        this.theOtherRunnableOnUseOrChosen = runnable;
 
         baseMagicNumber = magicNumber;
         initializeDescription();
     }
 
     public InariFlames(int magicNumber) {
-        super(NAME, DESCRIPTION, () -> {
+        super(NAME, DESCRIPTION, null);
+        Runnable runnable = ()->{
             AbstractPlayer p = AbstractDungeon.player;
             AbstractGameAction.AttackEffect effect = AbstractGameAction.AttackEffect.FIRE;
             DamageInfo.DamageType damageType = DamageInfo.DamageType.THORNS;
-            for (int i = 0; i < 10; i++){
-                atb(new DamageAction(m, new DamageInfo(p, 1+magicNumber, damageType), effect));
+            AbstractMonster m = GetAliveMonsterWithLessHealth();
+            if (m != null){
+                for (int i = 0; i < 10; i++){
+                    atb(new DamageAction(m, new DamageInfo(p, 1+magicNumber, damageType), effect));
+                }
             }
 
-        });
+        };
 
         baseMagicNumber = 1+magicNumber;
         initializeDescription();
+    }
+
+    @Override
+    public void onChoseThisOption() {
+        theOtherRunnableOnUseOrChosen.run();
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+        theOtherRunnableOnUseOrChosen.run();
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new EasyModalChoiceCard(NAME, DESCRIPTION, theOtherRunnableOnUseOrChosen);
+    }
+
+    private AbstractMonster GetAliveMonsterWithLessHealth(){
+        MonsterGroup monsterGroup = AbstractDungeon.getMonsters();
+        AbstractMonster monsterWithLessHealth = null;
+
+        for (int i = 0; i < monsterGroup.monsters.size(); i++){
+            /*
+            if monster is alive
+            etc
+             */
+            AbstractMonster currentMonster = monsterGroup.monsters.get(i);
+            if (!currentMonster.isDeadOrEscaped()){
+                if (monsterWithLessHealth == null){
+                    monsterWithLessHealth = currentMonster;
+                }
+                else if (currentMonster.currentHealth < monsterWithLessHealth.currentHealth)
+                {
+                    monsterWithLessHealth = currentMonster;
+                }
+            }
+        }
+
+        return monsterWithLessHealth;
     }
 }
