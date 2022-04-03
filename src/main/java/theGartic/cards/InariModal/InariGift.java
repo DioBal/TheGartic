2dc5b9579f5af70d91bc48f1a7bf6f96b76dbcf4
Cláuvin <Clauvin@users.noreply.github.com>
2022-04-03 12:55:42 -0300

    [SupplToInari] Inari Gift, v1

---
+++ src/main/java/theGartic/cards/InariModal/InariGift.java
@@ -0,0 +1,46 @@
+package theGartic.cards.InariModal;
+
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import com.megacrit.cardcrawl.actions.unique.DiscoveryAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import theGartic.cards.EasyModalChoiceCard;
+
+import static theGartic.GarticMod.makeID;
+import static theGartic.util.Wiz.atb;
+
+public class InariGift extends EasyModalChoiceCard {
+
+    /*
+    Step 1: Use Colorless Potion effect.
+    Step 2: Go after my old code for Rainbow Brush that creates cards and see how
+        to use it together with the Colorless Potion effect to make exhaustible Colorless cards.
+     */
+
+    public static final String ID = makeID(InariGift.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public InariGift(){
+        super(NAME, DESCRIPTION, () -> {
+            atb(new DiscoveryAction(true, 1));
+        });
+
+        baseMagicNumber = magicNumber;
+
+        initializeDescription();
+    }
+
+    public InariGift(int magicNumber){
+        super(NAME, DESCRIPTION, () -> {
+            atb(new DiscoveryAction(true, magicNumber));
+        });
+
+        baseMagicNumber = magicNumber;
+
+        initializeDescription();
+    }
+}
