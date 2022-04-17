aa1f2c632a968d4e5b6c42766f0090b138f21e98
Cláuvin <Clauvin@users.noreply.github.com>
2022-04-17 12:01:31 -0300

    [SupplToInari] Fixing InariCard not being abstract

--- src/main/java/theGartic/cards/InariModal/InariCard.java
+++ src/main/java/theGartic/cards/InariModal/InariCard.java
@@ -2,7 +2,7 @@ package theGartic.cards.InariModal;
 
 import theGartic.cards.EasyModalChoiceCard;
 
-public class InariCard extends EasyModalChoiceCard {
+public abstract class InariCard extends EasyModalChoiceCard {
 
     public InariCard(String name, String description, Runnable onUseOrChosen) {
         super(name, description, onUseOrChosen);
