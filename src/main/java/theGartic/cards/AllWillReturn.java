93f1be9e505e22fb4bfa16057073f74c90fd6db9
Bryan <BryanMKerns@gmail.com>
2022-04-23 17:22:07 -0500

    Fix #52 Fix #59

--- src/main/java/theGartic/cards/AllWillReturn.java
+++ src/main/java/theGartic/cards/AllWillReturn.java
@@ -17,49 +17,49 @@ public class AllWillReturn extends AbstractEasyCard {
 
     public AllWillReturn() {
         super(ID, 3, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        this.baseDamage = this.damage = this.baseBlock = this.block = 0;
+        baseDamage = damage = baseBlock = block = 0;
     }
 
     @Override
     public void applyPowers() {
-        this.baseBlock = lastTurnBlock;
-        this.baseDamage = lastTurnDamage;
+        baseBlock = lastTurnBlock;
+        baseDamage = lastTurnDamage;
         super.applyPowers();
 
-        this.rawDescription = cardStrings.EXTENDED_DESCRIPTION[0] + cardStrings.DESCRIPTION;
-        this.initializeDescription();
+        rawDescription = cardStrings.EXTENDED_DESCRIPTION[0] + cardStrings.DESCRIPTION;
+        initializeDescription();
     }
 
     @Override
     public void calculateCardDamage(AbstractMonster m) {
-        this.baseBlock = lastTurnBlock;
-        this.baseDamage = lastTurnDamage;
+        baseBlock = lastTurnBlock;
+        baseDamage = lastTurnDamage;
         super.calculateCardDamage(m);
 
-        this.rawDescription = cardStrings.EXTENDED_DESCRIPTION[0] + cardStrings.DESCRIPTION;
-        this.initializeDescription();
+        rawDescription = cardStrings.EXTENDED_DESCRIPTION[0] + cardStrings.DESCRIPTION;
+        initializeDescription();
     }
 
     public void onMoveToDiscard() {
-        this.rawDescription = cardStrings.DESCRIPTION;
-        this.initializeDescription();
+        rawDescription = cardStrings.DESCRIPTION;
+        initializeDescription();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (this.block > 0)
+        if (block > 0)
             blck();
 
-        if (this.damage >= 30) {
+        if (damage >= 30) {
             if (m != null) {
-                this.addToBot(new VFXAction(new WeightyImpactEffect(m.hb.cX, m.hb.cY)));
-                this.addToBot(new WaitAction(0.8F));
+                addToBot(new VFXAction(new WeightyImpactEffect(m.hb.cX, m.hb.cY)));
+                addToBot(new WaitAction(0.8F));
             }
             dmg(m, AbstractGameAction.AttackEffect.NONE);
         }
-        else if (this.damage >= 16) {
+        else if (damage >= 16) {
             dmg(m, AbstractGameAction.AttackEffect.SMASH);
         }
-        else if (this.damage >= 8) {
+        else if (damage >= 8) {
             dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
         }
         else {
