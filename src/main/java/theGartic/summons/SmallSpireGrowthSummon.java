4f5d8f057bbbef29dc7b63efab4f59024102a351
Cláuvin <Clauvin@users.noreply.github.com>
2022-04-23 22:42:20 -0300

    [CorruptedMeat] Started making SmallSpireGrowthSummon

---
+++ src/main/java/theGartic/summons/SmallSpireGrowthSummon.java
@@ -0,0 +1,23 @@
+package theGartic.summons;
+
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.OrbStrings;
+import theGartic.GarticMod;
+
+import static theGartic.GarticMod.makeOrbPath;
+
+public class SmallSpireGrowthSummon extends AbstractSummonOrb {
+
+    public static final String ORB_ID = GarticMod.makeID(SmallSpireGrowthSummon.class.getSimpleName());
+    private static final OrbStrings orbString = CardCrawlGame.languagePack.getOrbString(ORB_ID);
+    public static final String[] DESCRIPTIONS = orbString.DESCRIPTION;
+
+    public SmallSpireGrowthSummon()
+    {
+        super(ORB_ID, orbString.NAME, 1, 0, makeOrbPath("MischievousFox.png"));
+    }
+
+    //It should deal 5 points of damage at the end of the turn.
+    //How do I do that without much work...
+
+}
