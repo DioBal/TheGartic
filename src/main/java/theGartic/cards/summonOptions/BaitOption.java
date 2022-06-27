b1f84b8f4969e7c687dbd62e665ac8a3a0dea33a
Laugic <ajheb97@gmail.com>
2022-06-27 00:38:24 -0400

    Adding all summon options. Fixed Glass Armor. Fixed/finished Tall Grass. Finished Tamer's Candy. Added Spiked Collar.

--- src/main/java/theGartic/cards/summonOptions/BaitOption.java
+++ src/main/java/theGartic/cards/summonOptions/BaitOption.java
@@ -3,34 +3,33 @@ package theGartic.cards.summonOptions;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import theGartic.actions.SummonOrbAction;
-import theGartic.cards.Hunt;
-import theGartic.summons.HungryFoxSummon;
+import theGartic.summons.BaitSummon;
 
 import static theGartic.GarticMod.makeID;
 import static theGartic.util.Wiz.atb;
 
-public class HungryFoxOption extends AbstractSummonOption
+public class BaitOption extends AbstractSummonOption
 {
-    public static final String ID = makeID(HungryFoxOption.class.getSimpleName());
+    public static final String ID = makeID(BaitOption.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final int BASE_FOX_POWER = Hunt.MAGIC;
+    public static final int BASE_AMOUNT = BaitSummon.BASE_AMOUNT;
 
-    public HungryFoxOption()
+    public BaitOption()
     {
         this(true, false);
     }
 
-    public HungryFoxOption(boolean summon, boolean addToParty)
+    public BaitOption(boolean summon, boolean addToParty)
     {
         super(ID, CardType.ATTACK, summon, addToParty);
-        magicNumber = baseMagicNumber = BASE_FOX_POWER;
+        magicNumber = baseMagicNumber = BASE_AMOUNT;
     }
 
     @Override
     public void OnSummon()
     {
-        atb(new SummonOrbAction(new HungryFoxSummon(BASE_FOX_POWER)));
+        atb(new SummonOrbAction(new BaitSummon(BASE_AMOUNT)));
     }
 }
--- src/main/java/theGartic/cards/summonOptions/FetchingFoxOption.java
+++ src/main/java/theGartic/cards/summonOptions/FetchingFoxOption.java
@@ -3,34 +3,34 @@ package theGartic.cards.summonOptions;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import theGartic.actions.SummonOrbAction;
-import theGartic.cards.Hunt;
-import theGartic.summons.HungryFoxSummon;
+import theGartic.summons.DireWolfSummon;
+import theGartic.summons.FetchingFoxSummon;
 
 import static theGartic.GarticMod.makeID;
 import static theGartic.util.Wiz.atb;
 
-public class HungryFoxOption extends AbstractSummonOption
+public class FetchingFoxOption extends AbstractSummonOption
 {
-    public static final String ID = makeID(HungryFoxOption.class.getSimpleName());
+    public static final String ID = makeID(FetchingFoxOption.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final int BASE_FOX_POWER = Hunt.MAGIC;
+    public static final int BASE_AMOUNT = FetchingFoxSummon.BASE_AMOUNT;
 
-    public HungryFoxOption()
+    public FetchingFoxOption()
     {
         this(true, false);
     }
 
-    public HungryFoxOption(boolean summon, boolean addToParty)
+    public FetchingFoxOption(boolean summon, boolean addToParty)
     {
-        super(ID, CardType.ATTACK, summon, addToParty);
-        magicNumber = baseMagicNumber = BASE_FOX_POWER;
+        super(ID, CardType.SKILL, summon, addToParty);
+        magicNumber = baseMagicNumber = BASE_AMOUNT;
     }
 
     @Override
     public void OnSummon()
     {
-        atb(new SummonOrbAction(new HungryFoxSummon(BASE_FOX_POWER)));
+        atb(new SummonOrbAction(new FetchingFoxSummon(BASE_AMOUNT, BASE_AMOUNT)));
     }
 }
